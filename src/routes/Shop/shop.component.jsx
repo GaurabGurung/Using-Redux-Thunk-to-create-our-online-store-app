9fe53d2906d44b6830a70ac167f9735f266619d6
Gaurab Gurung <gaurabgurung181@gmail.com>
2023-11-08 22:26:15 +1100

    adding the full project from my another app

---
+++ src/routes/Shop/shop.component.jsx
@@ -0,0 +1,31 @@
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import { Route , Routes} from 'react-router-dom';
+import Category from '../category/category.component';
+import { useEffect } from 'react';
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+import { setCategories } from '../../store/categories/category.action'
+import { useDispatch } from 'react-redux';
+
+// import './shop.styles.scss';
+
+
+const Shop = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const getCategoriesMap = async ()=> {
+            const categoriesArray = await getCategoriesAndDocuments('categories');
+            dispatch(setCategories(categoriesArray));
+        }
+        getCategoriesMap();
+    }, [])
+    
+    return (
+        <Routes>
+            <Route index element={<CategoriesPreview/>}/>
+            <Route path=':category' element = {<Category/>}/>
+        </Routes>
+    )
+
+}
+export default Shop ; 
